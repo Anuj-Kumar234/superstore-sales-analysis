1f6ea697f15c38c855c3f28104fd12e90785a4fd
Anuj Kumar <anuj2430996@gmail.com>
2026-06-17 11:42:53 +0530

    Add reusable aggregation utilities

--- src/sales_analysis.py
+++ src/sales_analysis.py
@@ -1,147 +1,98 @@
-# total_recordsing total records and checking what columns are present there in the csv
-import csv
-from utils import top_n
+#Recording total records and checking what columns are present there in the csv
+from utils import top_n , load_data , aggregate
 
-
-with open("data/superstore.csv", "r", encoding="utf-8") as file:
-    reader = csv.reader(file)
-    total_records = 0
-    header = next(reader)
-    for i in reader:
-        total_records+= 1
-    print("Total records: ",total_records)
-    print(header)
-    print(len(header))
-
+data = load_data("data/superstore.csv")
+total_records = 0
+header = data[0]
+for i in data[1:]:
+    total_records+= 1
+print("Total records: ",total_records)
+print(header)
+print(len(header))
 
 # Total Sales
-with open("data/superstore.csv", "r", encoding="utf-8") as file:
-    reader = csv.reader(file)
-    total_sales = 0
-    next(reader)
-    for row in reader :
-        total_sales += float(row[-1])
-    print(f"Total Sales : ${total_sales:,.2f}")
+total_sales = 0
+    
+for row in data[1:] :
+    total_sales += float(row[-1])
+print(f"Total Sales : ${total_sales:,.2f}")
 
 
 
 #Average of sales
-with open("data/superstore.csv", "r", encoding="utf-8") as file:
-    reader = csv.reader(file)
-    average = total_sales / float(total_records)
-    print(f"Average : ${average:,.2f}")
+average = total_sales / float(total_records)
+print(f"Average : ${average:,.2f}")
 
 
 
 #Finding best performing category
-with open("data/superstore.csv", "r", encoding="utf-8") as file:
-    reader = csv.reader(file)
-    category_sales = {}
-    next(reader)
-    for row in reader:
-        category_sales[row[-4]] = category_sales.get(row[-4], 0) + float(row[-1])   #row[-1] is sales column and row[-4] is category column
+category_sales = aggregate(data, 14, 17)
+print("\nCategory Sales :")
+for category,sale in category_sales.items():
+    print(f"{category.title()}- ${sale:,.2f}")
     
-    print("\nCategory Sales :")
-    for category,sale in category_sales.items():
-        print(f"{category.title()}- ${sale:,.2f}")
-    
-    print("\nBest Category:")
-    best_category = max(category_sales,key=category_sales.get)
-    print(best_category)
+print("\nBest Category:")
+best_category = max(category_sales,key=category_sales.get)
+print(best_category)
 
 
 
 #Finding the best performing state
-with open("data/superstore.csv","r",encoding="utf-8") as file:
-    reader = csv.reader(file)
-    next(reader)
-    state_sales = {}
-    for row in reader:
-        state = row[10]
-        sales = row[-1]
-        state_sales[state] = state_sales.get(state,0) + float(sales)
-    
-    print('\nBest Performing State:')
-    best_performing_state = max(state_sales,key=state_sales.get)
+state_sales= aggregate(data,10,17)
 
-    print(f"{best_performing_state} - ${state_sales[best_performing_state]:,.2f}")
+print('\nBest Performing State:')
+best_performing_state = max(state_sales,key=state_sales.get)
 
-    # TOP 5 PERFORMING STATES
-    print("\nTop 5 Performing States:")
-    top_5 = top_n(state_sales,5)
-    for i,tup in enumerate(top_5):
-        print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
+print(f"{best_performing_state} - ${state_sales[best_performing_state]:,.2f}")
+
+# TOP 5 PERFORMING STATES
+print("\nTop 5 Performing States:")
+top_5 = top_n(state_sales,5)
+for i,tup in enumerate(top_5):
+    print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
     
 
 #Customers by Revenue
-with open("data/superstore.csv","r",encoding="utf-8") as file:
-    reader = csv.reader(file)
-    next(reader)
-    customer_revenue = {}
-    for row in reader:
-        name = row[6]
-        sales = row[-1]
-        customer_revenue[name] = customer_revenue.get(name,0) + float(sales)
+customer_revenue = aggregate(data, 6, 17)
 
     #Top 5 Customers by Revenue   
-    print("\nTop 5 Customers:")
-    top_customers = top_n(customer_revenue,5)
+print("\nTop 5 Customers:")
+top_customers = top_n(customer_revenue,5)
     
-    for i,tup in enumerate(top_customers):
-        print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
+for i,tup in enumerate(top_customers):
+    print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
 
 
 #Top Regions by Revenue
-with open("data/superstore.csv","r",encoding="utf-8") as file:
-    reader = csv.reader(file)
-    next(reader)
-    region_revenue = {}
-    for row in reader:
-        name = row[12]
-        sales = row[-1]
-        region_revenue[name] = region_revenue.get(name,0) + float(sales)
+region_revenue = aggregate(data, 12, 17)
 
-    #Top 5 Regions by Revenue   
-    print("\nTop 5 Regions:")
-    top_regions = top_n(region_revenue,5)
+#Top 5 Regions by Revenue   
+print("\nTop 5 Regions:")
+top_regions = top_n(region_revenue,5)
     
-    for i,tup in enumerate(top_regions):
-        print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
+for i,tup in enumerate(top_regions):
+    print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
 
 
 #Top Segments by Revenue
-with open("data/superstore.csv","r",encoding="utf-8") as file:
-    reader = csv.reader(file)
-    next(reader)
-    segments_revenue ={}
-    for row in reader:
-        name = row[7]
-        sales = row[-1]
-        segments_revenue[name] = segments_revenue.get(name,0) + float(sales)
-
-    #Top 5 Segments by Revenue   
-    print("\nTop 5 Segments:")
-    top_segments = top_n(segments_revenue,5)
+segments_revenue = aggregate(data,7,-1)
+#Top 5 Segments by Revenue   
+print("\nTop 5 Segments:")
+top_segments = top_n(segments_revenue,5)
     
-    for i,tup in enumerate(top_segments):
-        print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
+for i,tup in enumerate(top_segments):
+    print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
 
 #Top Products by Revenue
-with open("data/superstore.csv","r",encoding="utf-8") as file:
-    reader = csv.reader(file)
-    next(reader)
-    product_revenue = {}
-    for row in reader:
-        name = row[16]
-        sales = row[-1]
-        product_revenue[name] = product_revenue.get(name,0) + float(sales)
 
-    #Top 5 Products by Revenue   
-    print("\nTop 5 Products:")
-    top_products = top_n(product_revenue,5)
+product_revenue = aggregate(data,16,-1)
+
+#Top 5 Products by Revenue   
+print("\nTop 5 Products:")
+top_products = top_n(product_revenue,5)
     
-    for i,tup in enumerate(top_products):
-        print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
+for i,tup in enumerate(top_products):
+    print(f"{i+1}. {tup[0]} - ${tup[1]:,.2f}")
 
 print("\n===== BUSINESS INSIGHTS =====")
 
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,16 @@
+import csv
 def top_n(dict_1,n=5):
     return sorted(dict_1.items(),key=lambda x:x[1],reverse=True)[:n]
+
+def load_data(file_path: str) -> list[list[str]]:
+    with open(file_path, "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        return list(reader)
+
+def aggregate(data,key_index,value_index):
+    result = {}
+    for row in data[1:]:
+        key = row[key_index]
+        value = row[value_index]
+        result[key] = result.get(key,0) + float(value)
+    return result
